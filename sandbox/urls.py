73bc94cc9476bf16904bfe507b191d911d2b2604
Francis Reyes <francis.mark.reyes@gmail.com>
2014-08-04 15:53:32 +1000

    Update test settings

--- sandbox/urls.py
+++ sandbox/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     # Examples:
     # url(r'^$', 'sandbox.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
