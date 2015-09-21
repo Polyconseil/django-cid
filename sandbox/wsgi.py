1e2e3008d51e2ccc0741e1501d0436c7e49b5519
Jonathan Moss <jmoss@snowballone.com.au>
2015-09-21 10:47:51 +1000

    tox settings for Django 1.8

--- sandbox/wsgi.py
+++ sandbox/wsgi.py
@@ -1,5 +1,5 @@
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sandbox.settings")
 
-from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application  # noqa
 application = get_wsgi_application()
