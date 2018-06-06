14c97af35547ed419eb70b1284a856c8e93df7ec
Travis Thieman <travis.thieman@gmail.com>
2018-06-06 15:25:49 -0400

    Fork, rename, support Django 1.11

--- cid/__init__.py
+++ cid/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 
 default_app_config = 'cid.apps.CidAppConfig'
--- cid/backends/mysql/base.py
+++ cid/backends/mysql/base.py
@@ -5,6 +5,9 @@ from ...cursor import CidCursorWrapper
 
 class DatabaseWrapper(BaseMySQLWrapper):
 
-    def create_cursor(self):
-        base_cursor = super(DatabaseWrapper, self).create_cursor()
+    def create_cursor(self, name=None):
+        if name:
+            base_cursor = super(DatabaseWrapper, self).create_cursor(name)
+        else:
+            base_cursor = super(DatabaseWrapper, self).create_cursor()
         return CidCursorWrapper(base_cursor)
