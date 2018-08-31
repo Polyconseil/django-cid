58e36f6bbe1537a06565abf43770bd3dccd73a35
Damien Baty <damien.baty@polyconseil.fr>
2018-08-31 10:45:41 +0200

    apps: Remove translation of app name
    I am not sure where this verbose name appears, but I am pretty sure
    it's not useful to translate it. In fact it wasn't translated because
    there are no gettext (*.po) files.

--- cid/apps.py
+++ cid/apps.py
@@ -1,10 +1,6 @@
 from django.apps import AppConfig
-from django.utils.translation import ugettext_lazy as _
 
 
 class CidAppConfig(AppConfig):
-    """
-    Django 1.7+ application configuration
-    """
     name = 'cid'
-    verbose_name = _('Django Correlation Id')
+    verbose_name = 'Django Correlation Id'
