0fe4b1fd5736965dadc852bbfbc8d542b5f9f401
Damien Baty <damien.baty@polyconseil.fr>
2018-10-01 16:25:53 +0200

    Back to development: 1.2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ changelog = clean_history(open('HISTORY.rst').read())
 
 setup(
     name='django-cid',
-    version='1.1',
+    version='1.2.dev0',
     description="""Correlation IDs in Django for debugging requests""",
     long_description=readme + '\n\n' + changelog,
     author='Snowball One',
