8eba976ff9a7d24db072fdd6a2715d3f063cab2e
Sergei Shishov <sergei.shishov@dubizzle.com>
2019-08-28 11:02:48 +0400

    Add ability to override name in log record
    NOTE: documentation should be updated accordingly

--- cid/log.py
+++ cid/log.py
@@ -1,10 +1,12 @@
 import logging
 
+from django.conf import settings
+
 from cid.locals import get_cid
 
 
 class CidContextFilter(logging.Filter):
 
     def filter(self, record):
-        record.cid = get_cid()
+        setattr(record, getattr(settings, 'CID_LOG_RECORD_NAME', 'cid'), get_cid())
         return True
