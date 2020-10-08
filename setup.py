4bd427413d3ca0fde45c31bc19cd47330ae73eca
Xavier Fernandez <xavier.fernandez@polyconseil.fr>
2020-10-08 10:09:03 +0200

    setup: update minimum Django version

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     packages=find_packages(exclude=('sandbox*', 'tests*')),
     include_package_data=True,
     install_requires=[
-        'django>=1.11',
+        'django>=2.2',
     ],
     license="BSD",
     zip_safe=False,
@@ -45,5 +45,6 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 )
