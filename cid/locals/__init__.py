6c0b72cf5a959db6aa3744c9207fac9789f44245
Zeta <fernando@flieber.com>
2020-11-13 00:22:15 -0300

    Unpin isort and add changelog

--- cid/locals/__init__.py
+++ cid/locals/__init__.py
@@ -1,6 +1,6 @@
 try:
     # ContextVar is available since python 3.7,
-    # oldest python version keep using thread_local
+    # Older versions of Python will use thread_local
     from .context import set_cid, get_cid  # noqa
 except ImportError:
     from .thread_local import set_cid, get_cid  # noqa
