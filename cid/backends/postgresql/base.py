e9789dc2aeaecd656ab265de44c9abb41c62ac3e
Damien Baty <damien.baty@polyconseil.fr>
2021-01-21 20:31:22 +0100

    qa: Enable `useless-suppression` in pylint
    ... and do remove useless suppression.

--- cid/backends/postgresql/base.py
+++ cid/backends/postgresql/base.py
@@ -4,9 +4,6 @@ from ...cursor import CidCursorWrapper
 
 
 class DatabaseWrapper(BaseDatabaseWrapper):
-    # Don't warn about abstract `_start_transaction_under_autocommit`
-    # pylint: disable=abstract-method
-
     def create_cursor(self, name=None):
         base_cursor = super().create_cursor(name)
         return CidCursorWrapper(base_cursor)
