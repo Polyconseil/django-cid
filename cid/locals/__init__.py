Polyconseil/django-cid

commit c3a3b8a8adda6eb6321917c2f2dbad401f75ee80

qa: Add quality checks and fix pylint warnings

cid/locals/__init__.py
@@ -1,9 +1,11 @@
 try:
     # ContextVar is available since python 3.7,
     # Older versions of Python will use thread_local
-    from .context import set_cid, get_cid  # noqa
+    from .context import get_gid
+    from .context import set_cid
 except ImportError:
-    from .thread_local import set_cid, get_cid  # noqa
+    from .thread_local import get_cid
+    from .thread_local import set_cid
 
 from .base import generate_new_cid
 

Makefile
@@ -11,8 +11,12 @@ docs:
 	sphinx-build -W -n -b html docs ./build/sphinx/html
 
 quality:
-	python setup.py check --strict --metadata --restructuredtext
-	pylint --reports=no setup.py cid
+	check-manifest
+	isort --check-only --diff cid
+	pylint --reports=no setup.py cid tests
+	check-branches
+	check-fixmes
+	python setup.py sdist >/dev/null 2>&1 && twine check dist/*
 
 release: clean
 	fullrelease

tests/test_cursor.py
@@ -3,7 +3,6 @@ from unittest import mock
 from django.test import TestCase
 from django.test.utils import override_settings
 
-import cid.locals
 from cid.cursor import CidCursorWrapper
 
 

tests/test_middleware.py
@@ -1,4 +1,3 @@
-import unittest
 from unittest import mock
 
 from django.test import TestCase
@@ -23,11 +22,11 @@ def get_response(request):
     return DummyResponse(request)
 
 
-def make_request(cid=None, header_name='X_CORRELATION_ID'):
+def make_request(cid_=None, header_name='X_CORRELATION_ID'):
     request = mock.Mock()
     request.META = {}
-    if cid:
-        request.META[header_name] = cid
+    if cid_:
+        request.META[header_name] = cid_
     return request
 
 
@@ -46,7 +45,7 @@ class TestCidMiddleware(TestCase):
         self.assertEqual(response['X_CORRELATION_ID'], 'cid-from-upstream')
 
     def test_no_cid_from_upstream(self):
-        request = make_request(cid=None)
+        request = make_request(cid_=None)
         middleware = CidMiddleware(get_response=get_response)
         response = middleware(request)
         self.assertIsNone(request.correlation_id, None)
@@ -57,7 +56,7 @@ class TestCidMiddleware(TestCase):
     @mock.patch('uuid.uuid4')
     def test_generate_cid(self, uuid4):
         uuid4.return_value = 'generated-cid'
-        request = make_request(cid=None)
+        request = make_request(cid_=None)
         middleware = CidMiddleware(get_response=get_response)
         response = middleware(request)
         self.assertEqual(request.correlation_id, 'generated-cid')
@@ -68,7 +67,7 @@ class TestCidMiddleware(TestCase):
     @mock.patch('uuid.uuid4')
     def test_concatenate_ids(self, uuid4):
         uuid4.return_value = 'local-cid'
-        request = make_request(cid='upstream-cid')
+        request = make_request(cid_='upstream-cid')
         middleware = CidMiddleware(get_response=get_response)
         response = middleware(request)
         self.assertEqual(request.correlation_id, 'upstream-cid, local-cid')
@@ -114,9 +113,9 @@ class TestIntegration(TestCase):
 
         # A request without any correlation id
         response = self.client.get(url)
-        cid = response.get('X_CORRELATION_ID')
-        self.assertIsNotNone(cid)
+        cid_ = response.get('X_CORRELATION_ID')
+        self.assertIsNotNone(cid_)
 
         # A request *with* a correlation id
-        response = self.client.get(url, **{'X_CORRELATION_ID': cid})
-        self.assertEqual(response['X_CORRELATION_ID'], cid)
+        response = self.client.get(url, X_CORRELATION_ID=cid_)
+        self.assertEqual(response['X_CORRELATION_ID'], cid_)

commit d22355319c0c84316eefb6cd49c9ce0430bee05e

Fixes typo that caused "contextvar" to never be used
The typo was introduced in c3a3b8a8. I wonder why pylint did not warn
me about this unknown (and also unused) imported symbol.

Fixes #60.

cid/locals/__init__.py
@@ -1,7 +1,7 @@
 try:
     # ContextVar is available since python 3.7,
     # Older versions of Python will use thread_local
-    from .context import get_gid
+    from .context import get_cid
     from .context import set_cid
 except ImportError:
     from .thread_local import get_cid

docs/_static/css/custom.css
@@ -1,0 +1,3 @@
+.strike {
+    text-decoration: line-through;
+}

docs/conf.py
@@ -69,7 +69,6 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -100,6 +99,11 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_css_files = [
+    'css/custom.css',
+]
+
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.